cddfe642b5868db3c894d501c1412e61e0b9a85d
gepopa10 <gpopmescu@gmail.com>
2022-05-07 17:13:26 -0400

    set flask port in bash

--- robot_interface/src/server_flask_app.py
+++ robot_interface/src/server_flask_app.py
@@ -16,12 +16,10 @@ def update_odom(data):
 # The node is started in a separate thread to avoid conflicts with Flask.
 # The parameter *disable_signals* must be set if node is not initialized
 # in the main thread.
-Thread(target=lambda: rospy.init_node('robot_interface', disable_signals=True)).start()
+Thread(target=lambda: rospy.init_node('robot_interface_server', disable_signals=True)).start()
 
 # A subscriber to Odometry which is called when a message is received
 odom_subscriber = rospy.Subscriber("/odom", Odometry, update_odom)
-flask_app_server_port = rospy.get_param('/flask_app_server_port', 3000)
-
 app = Flask(__name__)
 
 
@@ -41,6 +39,7 @@ def get_robot_url():
 
 if __name__ == '__main__':
     try:
+        flask_app_server_port = rospy.get_param('/robot_interface_server/flask_app_server_port', 6000)
         app.run(debug=True, host='0.0.0.0', port=flask_app_server_port, threaded=True)
         # If we press control + C, the node will stop.
         rospy.spin()
--- robot_launch.sh
+++ robot_launch.sh
@@ -3,12 +3,13 @@
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
+flask_port=3001
+
 function ctrl_c() {
   echo "Stopping background ngrok process"
   kill -9 $(ps -ef | grep 'ngrok' | grep -v 'grep' | awk '{print $2}')
   echo "ngrok stopped"
   echo "Killing flask port"
-  flask_port=3000
   fuser -k ${flask_port}/tcp
   echo "Flask port $flask_port killed"
 }
@@ -16,4 +17,4 @@ function ctrl_c() {
 # launch ngrok in a terminal
 /bin/sh -ec 'ngrok tcp 22 --log=stdout > ngrok.log &'
 # launch robot system in another terminal
-/bin/sh -ec 'roslaunch turtlebot3_fake turtlebot3_path_dev_rviz.launch'
+/bin/sh -ec "roslaunch turtlebot3_fake turtlebot3_path_dev_rviz.launch flask_app_server_port:=${flask_port}"
