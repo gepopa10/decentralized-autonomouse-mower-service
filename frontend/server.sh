d1d76f0795721475475a189dd1fdaf3e40182bcd
gepopa10 <gpopmescu@gmail.com>
2022-05-07 21:26:24 -0400

    add comment

--- frontend/server.sh
+++ frontend/server.sh
@@ -7,17 +7,17 @@ address='0.0.0.0'
 port=4443
 
 function ctrl_c() {
+  fuser ${port}/tcp
   echo "Stopping background diode process"
   kill -9 $(ps -ef | grep 'diode' | grep -v 'grep' | awk '{print $2}')
   echo "diode stopped"
   echo "Stopping https server process"
   pkill -f https
   echo "https stopped"
-  fuser ${port}/tcp
 }
 
 # launch ngrok in a terminal
-/bin/sh -ec "python3 ./https.py --address=${address} --port=${port} &"
+/bin/sh -ec "python3 ./https.py --address=${address} --port=${port} &" #diode subdomain innacessible via https
 # /bin/sh -ec "python3 -m http.server ${port} &"
 # launch robot system in another terminal
 /bin/sh -ec "diode publish -public ${port}:80"
