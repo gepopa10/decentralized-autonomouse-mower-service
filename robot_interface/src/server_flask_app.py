8912f121da9251f903a3781a5f362e8b3c29d157
gepopa10 <gpopmescu@gmail.com>
2022-05-11 21:01:02 -0400

    camera

--- robot_interface/src/server_flask_app.py
+++ robot_interface/src/server_flask_app.py
@@ -4,6 +4,7 @@ import rospy
 from threading import Thread
 from flask import Flask, jsonify
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image
 from rospy_message_converter import json_message_converter
 
 odom = Odometry()
@@ -11,6 +12,13 @@ odom = Odometry()
 def update_odom(data):
     global odom
     odom = data
+
+
+image = Image()
+
+def update_image(data):
+    global image
+    image = data
 
 # ROS node, publisher, and parameter.
 # The node is started in a separate thread to avoid conflicts with Flask.
@@ -20,6 +28,9 @@ Thread(target=lambda: rospy.init_node('robot_interface_server', disable_signals=
 
 # A subscriber to Odometry which is called when a message is received
 odom_subscriber = rospy.Subscriber("/odom", Odometry, update_odom)
+# A subscriber to Image which is called when a message is received
+image_subscriber = rospy.Subscriber("/camera/rgb/image_raw", Image, update_image)
+
 app = Flask(__name__)
 
 
@@ -28,6 +39,19 @@ def get_odom():
     global odom
     json_str = json_message_converter.convert_ros_message_to_json(odom)
     return jsonify(odometry=json_str)
+
+
+@app.route('/get_image', methods = ['GET'])
+def get_image():
+    global image
+    json_str = json_message_converter.convert_ros_message_to_json(image)
+    # print(image.data)
+    # print("len(image.data)", len(image.data))
+    # print("height", image.height)
+    # print("width", image.width)
+    # print("step", image.step)
+    # j = {"data" : image.data}
+    return jsonify(test = [20, 30 ,40])
 
 
 @app.route('/get_robot_url', methods = ['GET'])
